cefb97424708fb17dd6b25d9d6b05c6c6e44cd53
Rounak Seth <sethrounak7@gmail.com>
2025-11-09 02:37:56 +0530

    add signup page

--- frontend-1/src/pages/signup/Signup.jsx
+++ frontend-1/src/pages/signup/Signup.jsx
@@ -1,9 +1,43 @@
-const Signup = () => {
-  return(
-    <div>
-      <h1>Signup</h1>
-    </div>
-  )
+import React from 'react';
+import { Form, useLoaderData, redirect, useNavigation, useActionData } from 'react-router-dom';
+import axios from 'axios';
+
+import './signup.css';
+
+async function action({ request }) {
+  const formData = await request.formData();
+  const username = formData.get('username');
+  const email = formData.get('email');
+  const password = formData.get('password');
+  const pathname = new URL(request.url).searchParams.get('redirectTo') || '/host';
+  try {
+    const res = await axios.post('http://localhost:3000/api/v1/login', { username, email, password });
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+    return err.message;
+  }
 }
 
-export default Signup;
+function SignupPage() {
+  const status = useNavigation().state;
+  const message = useLoaderData();
+  const err = useActionData();
+
+  return (
+    <div className='signup-container'>
+      <h1>Create your account</h1>
+      { message && <h3 className='red'>{ message }</h3> }
+      { err && <h3 className='red'>{ err }</h3> }
+      <Form method='post' replace={ true } className='signup-form'>
+          <input name='username' type='text' placeholder='Username'/>
+          <input name='email' type='email' placeholder='Email'/>
+          <input name='password' type='password' placeholder='Password'/>
+          <button disabled={ status === 'submitting' }>{ status === 'submitting' ? 'Signing up...' : 'Signup' }</button>
+      </Form>
+    </div>
+  );
+}
+
+SignupPage.action = action;
+export default SignupPage;
